8ddce50269081e2a62096207980fee7fcf541564
Mohammed Pasha <mk.pasha@hotmail.com>
2017-03-11 03:23:38 -0500

    Restoring repository

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Multitenant_Migration
-Project to create a program which will output User creation scripts, given a list of users and roles.
+Moving users from an isolated database to a pluggable container
+
+1. Extract Non-System user accounts from DB
+2. Provide List to Operations Teams - Nick/Sonia & Stela/Rebecca
+3. Produce CREATE commands from new/modified list
+
+Note: Currently attempt to complete the text reader to command application using HTML and JavaScript.
---
+++ script.js
@@ -0,0 +1,28 @@
+function stringParser() {
+    document.getElementById("data").innerHTML = "";
+    document.getElementById("script").innerHTML = "";
+    var password = "ILIKECHANNINGTATUM"
+    var documentInput = document.getElementById("input").value;
+
+    var splitPhase1 = documentInput.split(/ +|\r/);
+    splitPhase1.splice(-1,1);
+    var dataElements = splitPhase1.length;
+
+    if(dataElements/2 != 0) {
+
+    }
+
+    document.getElementById("data").innerHTML = splitPhase1 + "<br><br><br># of data elements:" + dataElements + "<br><br><br>";
+
+    for (i = 0; i < splitPhase1.length; i = i+2) {
+        document.getElementById("script").innerHTML = document.getElementById("script").innerHTML + "CREATE user " + splitPhase1[i] + " profile " + splitPhase1[i+1] + " identified by \"" + password + "\" default tablespace endusers temporary tablespace temporary quota unlimited on endusers account unlock;<br>";
+
+        if (splitPhase1[i+1] == "DEVELOPER") {
+            document.getElementById("script").innerHTML = document.getElementById("script").innerHTML + "GRANT appdeveloper, connect, sse_role to " + splitPhase1[i] + ";<br><br>";
+        }
+        else {
+            document.getElementById("script").innerHTML = document.getElementById("script").innerHTML + "GRANT connect, sse_role to " + splitPhase1[i] + ";<br><br>";
+        }
+    }
+
+}
