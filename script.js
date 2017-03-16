3fa05ef3bc5ec5b11e8f4f028b912b7961e362cc
Mohammed Pasha <mk.pasha@hotmail.com>
2017-03-16 10:37:31 -0400

    Add files via upload
    TEMPAPPDEVELOPER currently treated as ENDUSER
    Comments set aside DEVELOPER profile(when needed include comments to change to DEVELOPER profile)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function stringParser() {
     document.getElementById("data").innerHTML = "";
     document.getElementById("createCMD").innerHTML = "";
     document.getElementById("grantCMD").innerHTML = "";
-    
+
     var password = document.getElementById("pwd").value;
     var documentInput = document.getElementById("input").value;
 
@@ -13,9 +13,14 @@ function stringParser() {
     document.getElementById("data").innerHTML = splitPhase1 + "<br><br><br># of data element pairs:" + dataElements + "<br><br><br>";
 
     for (i = 0; i < splitPhase1.length; i = i+2) {
-        document.getElementById("createCMD").innerHTML = document.getElementById("createCMD").innerHTML + "CREATE user " + splitPhase1[i] + " profile " + splitPhase1[i+1] + " identified by \"" + password + "\" default tablespace endusers temporary tablespace temporary quota unlimited on endusers account unlock;<br><br>";
+        if (splitPhase1[i+1] == "TEMPAPPDEVELOPER") {
+            document.getElementById("createCMD").innerHTML = document.getElementById("createCMD").innerHTML + "CREATE user " + splitPhase1[i] + " profile ENDUSER" + /*DEVELOPER*/ " identified by \"" + password + "\" default tablespace endusers temporary tablespace temporary quota unlimited on endusers account unlock;<br><br>";
+        }
+        else {
+            document.getElementById("createCMD").innerHTML = document.getElementById("createCMD").innerHTML + "CREATE user " + splitPhase1[i] + " profile " + splitPhase1[i+1] + " identified by \"" + password + "\" default tablespace endusers temporary tablespace temporary quota unlimited on endusers account unlock;<br><br>";
+        }
 
-        if (splitPhase1[i+1] == "DEVELOPER") {
+        if (splitPhase1[i+1] == "DEVELOPER") {// || splitPhase1[i+1] == "TEMPAPPDEVELOPER") {
             document.getElementById("grantCMD").innerHTML = document.getElementById("grantCMD").innerHTML + "GRANT appdeveloper, connect, sse_role to " + splitPhase1[i] + ";<br><br>";
         }
         else {
